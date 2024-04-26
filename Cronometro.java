dade8224f03018e30b49e242110dccd262405043
Estefa-28 <stefyserrano977@gmail.com>
2024-04-26 17:11:49 -0500

    cronometro

---
+++ Cronometro.java
@@ -0,0 +1,43 @@
+
+package com.mycompany.dogs;
+
+public class Cronometro {
+    
+    public static void main(String[] args) {  
+        
+    for (int h=0; h<=24; h++){
+    
+        for (int i=0; i<=60;i++){
+            
+             for (int x=0; x<=60; x++){
+               System.out.println(h +":"+i+":"+ x);
+             }          
+         
+        }
+        
+    }
+        
+        
+        
+        
+    }
+  
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+}
